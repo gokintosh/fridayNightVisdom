9151361a43c49eace112be5651415f4a057009b4
gokintosh <gokul.kannambally_balachandran@student.po.edu.pl>
2023-06-24 00:08:41 +0200

    24th june

--- javatuts/src/main/java/org/gokul/streams/StreamsIntro.java
+++ javatuts/src/main/java/org/gokul/streams/StreamsIntro.java
@@ -1,8 +1,8 @@
 package org.gokul.streams;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamsIntro {
 
@@ -54,12 +54,49 @@ public class StreamsIntro {
 //    noneMatch, findFirst and findAny are short-circuiting terminal operations.
 
     public static void main(String[] args) {
-
-        List<Integer>list= Arrays.asList(1,2,3,11,5,6,20,15);
-
-        System.out.println(findSumOfNumbersGreaterThan10(list));
-
 //
+//        List<Integer>list= Arrays.asList(1,2,3,11,5,6,20,15);
+//
+//        System.out.println(findSumOfNumbersGreaterThan10(list));
+//
+////      creating java streams
+////      there are several ways to create a stream.
+////        using Stream.of()
+//
+//        Stream<Integer> stream=Stream.of(1,2,3,4,5);
+//
+////        we can also create streams from arrays but no primitive type is accepted(no autoboxing)
+//
+//        Stream<Integer> stream1=Stream.of(new Integer[]{1,2,3,4});
+//
+////        we can use Collection stream() to create sequential stream and parallelStream() to create parallel stream.
+//        List<Integer> list1=new ArrayList<>();
+//        for(int i=0;i<10;i++){
+//            list1.add(i);
+//        }
+//
+////        creating stream
+//        Stream<Integer>stream2=list1.stream();
+////        creating parallelStream
+//        Stream<Integer>stream3=list1.parallelStream();
+//
+////        we can also create streams using generate method
+//        Stream<String> stream4=Stream.generate(()-> "ac").limit(10);
+//        stream4.forEach(System.out::println);
+
+//        creating collections from stream
+        Stream<Integer>integerStream=Stream.of(1,2,3,4,5,6,7);
+//        List<Integer>integerList= integerStream.toList();
+//        integerList.forEach(System.out::println);
+//        Map<Integer,Integer>integerMap=new HashMap<>();
+//        integerMap=integerStream.collect(Collectors.toMap(i->i,i->i+10));
+//
+//        integerMap.forEach((integer, integer2) -> System.out.println(integer+" "+integer2));
+
+//        java streams intermediate operations
+
+
+
 
     }
 }
